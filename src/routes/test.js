f203d3f807e2aa9a0af4c96d3e4359cae5f223af
augrim888 <augrim-adhikari@uiowa.edu>
2021-10-19 10:09:49 -0500

    databased

--- src/routes/test.js
+++ src/routes/test.js
@@ -7,9 +7,9 @@ exports.getTest = (req,res) => {
 
     const connection = mysql.createPool({
         host     : 'localhost',
-        user     : 'root',
+        user     : 'spencer',
         port: 3306,
-        password : 'Europ@123!',
+        password : 'password123',
         database : 'opticx',
         connectionLimit : 0,
         queueLimit : 0,
