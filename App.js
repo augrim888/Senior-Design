9895ad5ffe1520d978b1efd7a0bb6c2883b3c1c3
augrim888 <augrim-adhikari@uiowa.edu>
2021-11-14 15:21:22 -0600

    app

--- App.js
+++ App.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNav from './src/routes/StackNav';
+import 'react-native-gesture-handler';
+// Import Navigators from React Navigation
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../screens/pickScreen';
+import Login from '../screens/loginScreen';
+import Signup from '../screens/Signup'
+import React from 'react';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const Auth = () => {
+  // Stack Navigator for Login and Sign up Screen
   return (
-  <NavigationContainer>
-    <StackNav/>
-  </NavigationContainer>);}
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{headerShown: true}}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{
+          title: 'Signup', //Set Header Title
+          headerStyle: {
+            backgroundColor: '#307ecc', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Auth Navigator: Include Login and Signup */}
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: false}}
+        />
+        {/* Navigation Drawer as a landing page */}
+        <Stack.Screen
+          name="DrawerNavigationRoutes"
+          component={DrawerNavigationRoutes}
+          // Hiding header for Navigation Drawer
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
