3127a3603a0f430d8aac58ace0f773a7d2ba0081
sliebermann <augrim-adhikari@uiowa.edu>
2021-11-14 12:46:42 -0600

    added register

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,13 +4,14 @@ const mysql = require('mysql');
 
 const connection = mysql.createPool({
   host     : 'localhost',
-  user     : 'opticx',
-  password : 'password',
-  database : 'opticx'
+  user     : 'root',
+  password : 'Europ@123!',
+  database : 'seniordesign',
 });
 
 const app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 // Creating a GET route that returns data from the 'users' table.
 app.get('/user_info', function (req, res) {
     // Connecting to the database.
@@ -25,6 +26,30 @@ app.get('/user_info', function (req, res) {
       res.send(results)
     });
   });
+});
+
+app.post('/login', function (req, res) {
+  // Connecting to the database.
+  connection.getConnection(function (err, connection) {
+
+  // Executing the MySQL query (select all data from the 'users' table).
+  connection.query('SELECT * FROM user_info WHERE user = "' + req.body.user + '"', function (error, results, fields) {
+    //console.log('SELECT * FROM user_info WHERE user = "' + req.body.user + '"')
+    // If some error occurs, we throw an error.
+    if (error) throw error;
+    console.log(results[0].user)
+    if(req.body.password == results[0].password)
+    {
+      returnstatus = "LOGIN SUCCESS"
+    }
+    else
+    {
+      returnstatus = "LOGIN FAILED"
+    }
+    // Getting the 'response' from the database and sending it to our route. This is were the data is.
+    res.send(returnstatus)
+  });
+});
 });
 
 // Starting our server.
