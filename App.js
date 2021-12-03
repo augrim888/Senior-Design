466e54c60b1d498b3b1a94da9c8c9db8bddbbb47
‘Ephraim <malikystore@gmail.com>
2021-12-02 22:35:24 -0600

    commit try

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Optic_User_Interface from './screen/Optic_Uploads_Center'
+import Optic_User_Interfa from './screen/Optic_Uploads_Center'
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Optic_User_Interface/>
+      <Optic_User_Interfa/>
       <StatusBar style="auto" />
     </View>
   );
